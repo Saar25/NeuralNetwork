52723fcf47b4ec8539412e3808b1929dbfda11ed
Saar <notnow@maybelater.com>
2020-03-25 19:36:43 +0200

    Created EvaluatorsGeneration

--- src/main/java/games/game1010/EvaluatorsGeneration.java
+++ src/main/java/games/game1010/EvaluatorsGeneration.java
@@ -1,12 +1,16 @@
 package games.game1010;
 
+import games.GenerationSettings;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class EvaluatorsGeneration {
 
     private final List<BoardEvaluator> evaluators = new ArrayList<>();
+    private final GenerationSettings generationSettings;
 
-    public EvaluatorsGeneration(int evaluatorsPerGeneration)
-
+    public EvaluatorsGeneration(GenerationSettings generationSettings) {
+        this.generationSettings = generationSettings;
+    }
 }
