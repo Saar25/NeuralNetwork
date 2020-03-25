7477cdcb9fa17bfdbcd71608079611934d4e16c7
Saar <notnow@maybelater.com>
2020-03-25 18:08:16 +0200

    Added Cell functions

--- src/main/java/games/game1010/Shape.java
+++ src/main/java/games/game1010/Shape.java
@@ -18,16 +18,29 @@ public class Shape {
         return cells;
     }
 
-    public boolean draw(Board board, Position position) {
+    public boolean canBePlaced(Board board, Position position) {
         if (board.exist(position) && board.exist(position.add(getCells().length, getCells()[0].length))) {
             for (int row = 0; row < getCells().length; row++) {
                 for (int col = 0; col < getCells()[row].length; col++) {
-                    final Position current = new Position(row, col);
-                    if (board.hasCell())
+                    final Position current = position.add(row, col);
+                    if (board.hasCell(current)) {
+                        return false;
+                    }
                 }
             }
         } else {
             return false;
+        }
+        return true;
+    }
+
+    public boolean place(Board board, Position position) {
+        for (int row = 0; row < getCells().length; row++) {
+            for (int col = 0; col < getCells()[row].length; col++) {
+                final Cell cell = getCells()[row][col] ? new Cell() : null;
+                final Position current = position.add(row, col);
+                board.setCell(current, cell);
+            }
         }
     }
 }
