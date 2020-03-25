e229d0ca151a228194057ab26517da49a04ab4d3
Saar <notnow@maybelater.com>
2020-03-25 02:19:26 +0200

    Moved to resources

--- src/main/java/games/game1010/ShapesReader.java
+++ src/main/java/games/game1010/ShapesReader.java
@@ -1,9 +1,22 @@
 package games.game1010;
 
+import com.google.gson.Gson;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Arrays;
+
 public class ShapesReader {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        final Gson gson = new Gson();
 
+        final File file = new File("shapes.json");
+        System.out.println(file.exists());
+        final Shape shape = gson.fromJson(new FileReader(file), Shape.class);
+
+        System.out.println(Arrays.deepToString(shape.getCells()));
     }
 
 }
