b5954461e7c6254e9897599c619b0981a2256ce3
Saar <notnow@maybelater.com>
2020-03-26 23:24:15 +0200

    Removed imports

--- src/main/java/games/game1010/Game1010.java
+++ src/main/java/games/game1010/Game1010.java
@@ -7,8 +7,6 @@ import neural.MatrixNeuralNetwork;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class Game1010 {
 
