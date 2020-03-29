db7851d62ae1e3084c62ae164f55fa147edc1d82
Saar <notnow@maybelater.com>
2020-03-29 15:49:32 +0300

    Optimized GuiPainter

--- src/main/java/games/game1010/painters/GuiPainter.java
+++ src/main/java/games/game1010/painters/GuiPainter.java
@@ -3,6 +3,7 @@ package games.game1010.painters;
 import games.game1010.Board;
 import games.game1010.Cell;
 import games.game1010.Position;
+import games.gui.Drawable;
 import games.gui.Renderer;
 import games.gui.Window;
 
@@ -10,31 +11,41 @@ import java.awt.*;
 
 public class GuiPainter implements BoardPainter {
 
+    private final Window window;
     private final Renderer renderer;
     private final int cellSize;
 
+    private Board board;
+
     public GuiPainter(Window window, int cellSize) {
+        this.window = window;
         this.cellSize = cellSize;
         this.renderer = new Renderer();
         window.add(renderer);
+        renderer.add(this::draw);
     }
 
     @Override
     public void paint(Board board) {
-        renderer.add(g -> {
-            final Color[] colors = {Color.WHITE, Color.BLACK};
-
-            for (int row = 0; row < board.getSize(); row++) {
-                for (int col = 0; col < board.getSize(); col++) {
-                    final Position current = new Position(row, col);
-                    final Cell cell = board.getCell(current);
-                    final Color color = colors[cell == null ? 0 : 1];
-
-                    paintCell(g, current, color);
-                }
-            }
-        });
+        this.board = board;
         renderer.repaint();
+    }
+
+    private void draw(Graphics g) {
+        if (board == null) {
+            return;
+        }
+        final Color[] colors = {Color.WHITE, Color.BLACK};
+
+        for (int row = 0; row < board.getSize(); row++) {
+            for (int col = 0; col < board.getSize(); col++) {
+                final Position current = new Position(row, col);
+                final Cell cell = board.getCell(current);
+                final Color color = colors[cell == null ? 0 : 1];
+
+                paintCell(g, current, color);
+            }
+        }
     }
 
     private void paintCell(Graphics g, Position position, Color color) {
